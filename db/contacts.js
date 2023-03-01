839972c825b929fd64ab7b334eaa89b0cc8cc910
DmitryNikolin <ni-ko-dim@ukr.net>
2023-03-01 14:04:25 +0200

    Initial commit

---
+++ db/contacts.js
@@ -0,0 +1,77 @@
+const fs = require("fs").promises;
+const path = require("path");
+const { nanoid } = require("nanoid");
+// import { v4 as generateId } from 'uuid';
+
+const contactsPath = path.resolve("./db/contacts.json");
+
+// # Get and display the entire list of contacts in the form of a table (console.table): node index.js --action list
+
+const listContacts = async () => {
+  try {
+    const data = await fs.readFile(contactsPath, "utf-8");
+    return JSON.parse(data);
+  } catch (error) {
+    console.error(error.message.red);
+  }
+};
+
+// # Get contact by id: node index.js --action get --id 5
+
+const getContactById = async (id) => {
+  const contactsArr = await listContacts();
+  const contact = contactsArr.find((element) => element.id === id);
+  if (!contact) {
+    return null;
+  }
+  return contact;
+};
+
+// # Remove contact using id: node index.js --action remove --id 3
+
+const removeContact = async (id) => {
+  try {
+    const contactsArr = await listContacts();
+    const itemIndex = contactsArr.findIndex((element) => element.id === id);
+    if (itemIndex === -1) {
+      return null;
+    }
+    contactsArr.splice(itemIndex, 1);
+    await fs.writeFile(contactsPath, JSON.stringify(contactsArr));
+    console.table(contactsArr);
+    return console.log(
+      "Contact with id: ".blue + id.yellow + " was removed".blue
+    );
+  } catch (error) {
+    console.log(error.red);
+  }
+};
+
+// # Add a contact: node index.js --action add --name Mango --email mango@gmail.com --phone 322-22-22
+
+const addContact = async (name, email, phone) => {
+  try {
+    const contactsArr = await listContacts();
+
+    const newContact = {
+      id: nanoid(),
+      name,
+      email,
+      phone,
+    };
+
+    contactsArr.push(newContact);
+    await fs.writeFile(contactsPath, JSON.stringify(contactsArr));
+    console.table(contactsArr);
+    console.log("Contact: ".blue + name.yellow + " was added to contacts".blue);
+  } catch (error) {
+    console.log(error.red);
+  }
+};
+
+module.exports = {
+  listContacts,
+  getContactById,
+  addContact,
+  removeContact,
+};
---
+++ index.js
@@ -0,0 +1,47 @@
+const { Command } = require("commander");
+const program = new Command();
+
+require("colors");
+
+const {
+  listContacts,
+  getContactById,
+  addContact,
+  removeContact,
+} = require("./db/contacts");
+
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+
+const argv = program.opts();
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case "list":
+      return console.table(await listContacts());
+      break;
+
+    case "get":
+      return console.table(await getContactById(id));
+      break;
+
+    case "add":
+      return console.table(await addContact(name, email, phone));
+      break;
+
+    case "remove":
+      return console.table(await removeContact(id));
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+};
+
+invokeAction(argv);
